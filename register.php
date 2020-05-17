7d444eaa2ce0078b8e41742496cdd4b0b9953ef3
YASH01009 <46935127+YASH01009@users.noreply.github.com>
2020-05-17 16:04:49 +0530

    edited

--- register.php
+++ register.php
@@ -33,9 +33,9 @@
 
 <?php
 
-$username = 'id13737626_mydb';
-$dbpassword = 'g<W>rc&D=tH9zbNR';
-$dbname = 'id13737626_studentdb';
+$username = '';
+$dbpassword = '';
+$dbname = '';
 
 $dbc = mysqli_connect('localhost',$username,$dbpassword,$dbname) or die('Error connecting to MySql Server');
 
